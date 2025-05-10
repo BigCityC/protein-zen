319368a3a936e5e30e78da10956e38783305812e
Andrew Aramouni <aramouni8@gmail.com>
2025-05-10 15:49:51 -0400

    testing

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+// import { Link, NavLink } from "react-router";
+import "./Navbar.css";
+
+export default function Navbar({ user }) {
+    return (<div className="navbar">
+        <nav className="nav-wrapper">
+            <div className="macrozen-title">MacroZen</div>
+            {/* <Link to="/" className="btn btn-nav"></Link> */}
+        </nav>
+    </div>);
+}
